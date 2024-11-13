28d14686d6042a61a7c5e8318053980a7d29aa41
Andrii Dmytrenko <dmitrenko1567@gmail.com>
2024-11-13 21:36:02 +0100

    Lesson25

---
+++ src/Lesson25.java
@@ -0,0 +1,12 @@
+public class Lesson25 {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.eat();
+        animal.sleep();
+        Dog hanny = new Dog();
+        hanny.eat();
+        hanny.sleep();
+        hanny.bark();
+        hanny.showName();
+    }
+}
